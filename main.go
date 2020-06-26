7edea86cdf54a08b8050a68c6af4e44c649113c6
Gianni Massi <me@giannimassi.it>
2020-06-26 19:20:15 +0200

    refactor: separate file for ui

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
 	"os"
-
-	"github.com/eiannone/keyboard"
-	"github.com/gosuri/uilive"
 )
 
 func main() {
@@ -16,87 +12,13 @@ func main() {
 }
 
 func run(args []string) error {
-	// Init state
+	// Get branches
 	view := &branchesState{}
 	if err := view.init(args); err != nil {
 		return err
 	}
 
-	// Setup key input
-	keyCh, err := keyboard.GetKeys(10)
-	if err != nil {
-		return err
-	}
-	defer keyboard.Close()
-
-	// Setup writer and start with currently selected branch
-	stdout := uilive.New()
-	fmt.Fprintf(stdout, "→	%s\n", view.selectedBranchWithColor())
-	stdout.Flush()
-
-	// Listen for keyboard events and term signal
-	for {
-		select {
-		case ev := <-keyCh:
-			done, err := handleKeyEvent(stdout, view, ev)
-			if !done {
-				continue
-			}
-			return err
-		}
-	}
-}
-
-func handleKeyEvent(wf writerFlusher, view *branchesState, ev keyboard.KeyEvent) (bool, error) {
-	if ev.Err != nil {
-		return true, ev.Err
-	}
-	switch {
-	// Exit
-	case ev.Key == keyboard.KeyCtrlC:
-		fallthrough
-	case ev.Key == keyboard.KeyEsc:
-		return true, nil
-
-	// Up
-	case ev.Key == keyboard.KeyArrowUp:
-		fallthrough
-	case ev.Key == keyboard.KeyArrowLeft:
-		fallthrough
-	case ev.Rune == 'h':
-		view.selectPrevious()
-		fmt.Fprintf(wf, "→	%s\n", view.selectedBranchWithColor())
-		wf.Flush()
-
-	// Down
-	case ev.Key == keyboard.KeyArrowDown:
-		fallthrough
-	case ev.Key == keyboard.KeyArrowRight:
-		fallthrough
-	case ev.Rune == 'j':
-		view.selectNext()
-		fmt.Fprintf(wf, "→	%s\n", view.selectedBranchWithColor())
-		wf.Flush()
-
-	// Enter
-	case ev.Key == keyboard.KeyEnter:
-		return true, cmdRun("git", "checkout", extractBranch(view.selectedBranchName()))
-
-	default:
-		return false, nil
-	}
-	return false, nil
-}
-
-type writerWrapper struct {
-	io.Writer
-}
-
-func (w *writerWrapper) Flush() error {
-	return nil
-}
-
-type writerFlusher interface {
-	io.Writer
-	Flush() error
+	// Run ui
+	ui := newUI(view)
+	return ui.run(context.Background())
 }
---
+++ ui.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/eiannone/keyboard"
+	"github.com/gosuri/uilive"
+)
+
+type ui struct {
+	wf   writerFlusher
+	view *branchesState
+}
+
+func newUI(view *branchesState) ui {
+	return ui{
+		wf:   uilive.New(),
+		view: view,
+	}
+}
+
+func (ui *ui) run(ctx context.Context) error {
+	// Setup key input
+	keyCh, err := keyboard.GetKeys(10)
+	if err != nil {
+		return err
+	}
+	defer keyboard.Close()
+
+	// Listen for keyboard events
+	for {
+		select {
+		case ev := <-keyCh:
+			done, err := ui.handleKeyEvent(ev)
+			if !done {
+				continue
+			}
+			return err
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (ui *ui) handleKeyEvent(ev keyboard.KeyEvent) (bool, error) {
+	if ev.Err != nil {
+		return true, ev.Err
+	}
+	switch {
+	// Exit
+	case ev.Key == keyboard.KeyCtrlC:
+		fallthrough
+	case ev.Key == keyboard.KeyEsc:
+		return true, nil
+
+	// Up
+	case ev.Key == keyboard.KeyArrowUp:
+		fallthrough
+	case ev.Key == keyboard.KeyArrowLeft:
+		fallthrough
+	case ev.Rune == 'h':
+		ui.view.selectPrevious()
+		fmt.Fprintf(ui.wf, "→	%s\n", ui.view.selectedBranchWithColor())
+		ui.wf.Flush()
+
+	// Down
+	case ev.Key == keyboard.KeyArrowDown:
+		fallthrough
+	case ev.Key == keyboard.KeyArrowRight:
+		fallthrough
+	case ev.Rune == 'j':
+		ui.view.selectNext()
+		fmt.Fprintf(ui.wf, "→	%s\n", ui.view.selectedBranchWithColor())
+		ui.wf.Flush()
+
+	// Enter
+	case ev.Key == keyboard.KeyEnter:
+		return true, cmdRun("git", "checkout", extractBranch(ui.view.selectedBranchName()))
+
+	default:
+		return false, nil
+	}
+	return false, nil
+}
+
+type writerFlusher interface {
+	io.Writer
+	Flush() error
+}
